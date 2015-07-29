ac09a9d4065fafff6603230f5760278e03409005
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2015-07-30 04:33:20 +0900

    管理画面を回収

--- app/controllers/admin/goods_controller.rb
+++ app/controllers/admin/goods_controller.rb
@@ -5,9 +5,24 @@ class Admin::GoodsController < ApplicationController
 
   end
 
+  def edit
+    @good = Good.find params[:id]
+  end
+
+  def update
+    @good = Good.find params[:id]
+    @good.update_attributes(good_params)
+    redirect_to admin_goods_path
+  end
+
   def destroy
     good = Good.find params[:id]
     good.destroy
-    redirect_to :back
+    redirect_to admin_goods_path
+  end
+
+  private
+  def good_params
+    params.require(:good).permit(:value)
   end
 end
